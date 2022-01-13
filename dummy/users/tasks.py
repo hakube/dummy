21f5eb55bdff4d70fc7ab2a40b4f6527702c26d0
hakube <hakube@outlook.com>
2022-01-13 08:20:53 +0800

    Initial commit

---
+++ dummy/users/tasks.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+
+from config import celery_app
+
+User = get_user_model()
+
+
+@celery_app.task()
+def get_users_count():
+    """A pointless Celery task to demonstrate usage."""
+    return User.objects.count()
+
+@celery_app.task()
+def get_users_list():
+    """A pointless Celery task to demonstrate usage."""
+    return [user.username for user in User.objects.all()]
