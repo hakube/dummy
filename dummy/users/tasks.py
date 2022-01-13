c44a47a501bc17fe82cd76fbf9be69c10b8b8251
hakube <hakube@outlook.com>
2022-01-13 10:11:49 +0800

    Redeploy

--- dummy/users/tasks.py
+++ dummy/users/tasks.py
@@ -14,3 +14,8 @@ def get_users_count():
 def get_users_list():
     """A pointless Celery task to demonstrate usage."""
     return [user.username for user in User.objects.all()]
+
+@celery_app.task()
+def get_users_list_with_id():
+    """A pointless Celery task to demonstrate usage."""
+    return [(user.username, user.id) for user in User.objects.all()]
